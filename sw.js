c7ee6507538e66cc6814b95d1118f43f08be09cd
jbcharles14-lab <jb.charles14@gmail.com>
2026-08-31 20:21:48 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,32 @@
+const CACHE = "maxhangs-v2";
+const FICHIERS = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icone-192.png",
+  "./icone-512.png",
+  "./icone-maskable.png"
+];
+
+self.addEventListener("install", e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FICHIERS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener("activate", e => {
+  e.waitUntil(
+    caches.keys()
+      .then(ks => Promise.all(ks.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener("fetch", e => {
+  if(e.request.method !== "GET") return;
+  e.respondWith(
+    caches.match(e.request).then(rep => rep || fetch(e.request).then(net => {
+      const copie = net.clone();
+      caches.open(CACHE).then(c => c.put(e.request, copie)).catch(()=>{});
+      return net;
+    }).catch(() => caches.match("./index.html")))
+  );
+});
